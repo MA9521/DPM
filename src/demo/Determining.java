2d02ab48ca685ceb106ff0bb455f11f4b53a826f
MA9521 <46541787+MA9521@users.noreply.github.com>
2019-02-24 18:03:24 -0500

    Add files via upload

---
+++ src/demo/Determining.java
@@ -0,0 +1,128 @@
+package demo;
+
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.lcd.TextLCD;
+
+public class Determining {
+	private static Port portColorCan= LocalEV3.get().getPort("S4");
+	  private static SensorModes myColorCan= new EV3ColorSensor(portColorCan);
+	  private static SampleProvider myColorStatusCan = myColorCan.getMode("RGB");
+	  private static float[] sampleColorCan= new float[myColorStatusCan.sampleSize()];
+	  private static final int canDi=3;
+	  
+	  private static final EV3LargeRegulatedMotor colorMotor =new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
+	  
+	  private static Port portUS= LocalEV3.get().getPort("S1");
+	  private static SensorModes myUS =new EV3UltrasonicSensor(portUS);
+	  private static SampleProvider myDistance= myUS.getMode("Distance");
+	  private static float[] sampleUS= new float[myDistance.sampleSize()];
+	  private static int wallDist;
+	  
+	  public static void main(String[] args){
+		  TextLCD lcd= LocalEV3.get().getTextLCD();
+		  for (int i=0;i<10;i++) { //take 10 dummy measurements to initialize the ultrasonic sensor
+		      myDistance.fetchSample(sampleUS,0);
+		      wallDist= (int)(sampleUS[0]*100.0);
+		    }
+		  while(true) {
+			  lcd.clear();
+		  Button.waitForAnyPress();
+		  myDistance.fetchSample(sampleUS,0);
+	      wallDist= (int)(sampleUS[0]*100.0);
+		  while(wallDist>canDi) {
+			  myDistance.fetchSample(sampleUS,0);
+		      wallDist= (int)(sampleUS[0]*100.0);
+		  }
+		  int counter=0;
+		  lcd.drawString("Object Detected",0,counter);
+		  counter++;
+		  
+		  
+		    double rMean=0; double gMean=0; double bMean=0;
+		    int i=0;
+		    long correctionStart, correctionEnd;
+		    colorMotor.setSpeed(36); //will take 5 seconds to turn 180 degrees
+		    colorMotor.rotate(170,true);
+		    while(i<20) {
+		      correctionStart = System.currentTimeMillis();
+		      myColorStatusCan.fetchSample(sampleColorCan,0);
+		      double intem0=sampleColorCan[0];
+		      double intem1=sampleColorCan[1];
+		      double intem2=sampleColorCan[2];
+		      double norm= Math.sqrt(intem0*intem0+intem1*intem1+intem2*intem2);
+		      
+		      rMean+=intem0/norm;
+		      gMean+=intem1/norm;
+		      bMean+=intem2/norm;
+		      
+		      i++;
+		      Sound.beep();
+		      correctionEnd = System.currentTimeMillis();
+		      if (correctionEnd - correctionStart < 200) { //take a measurement every 400 ms
+		        try {
+		          Thread.sleep(200 - (correctionEnd - correctionStart)); 
+		        } catch (InterruptedException e) {
+		          // there is nothing to be done here
+		        }
+		      }
+		      
+		      
+		    }
+		    
+		    
+		    
+		    rMean/=20; gMean/=20; bMean/=20;
+		    
+		    float[] blueAverages= {0.48f,0.56f,0.62f};float[] blueStd= {0.16f,0.14f,0.18f};
+		    float[] greenAverages= {0.50f,0.74f,0.39f};float[] greenStd= {0.12f,0.08f,0.18f};
+		    float[] yellowAverages= {0.86f,0.38f,0.29f};float[] yellowStd= {0.05f,0.05f,0.18f};
+		    float[] redAverages= {0.98f,0.11f,0.14f};float[] redStd= {0.01f,0.04f,0.05f};
+		    
+		    if(Math.abs(rMean-blueAverages[0])<=2*blueStd[0] &&  Math.abs(gMean-blueAverages[1])<=2*blueStd[1] && Math.abs(bMean-blueAverages[2])<=2*blueStd[2]) {
+		    	if((rMean-blueAverages[0])*(rMean-blueAverages[0])+(gMean-blueAverages[1])*(gMean-blueAverages[1])+(bMean-blueAverages[2])*(bMean-blueAverages[2])
+		    			<(rMean-greenAverages[0])*(rMean-greenAverages[0])+(gMean-greenAverages[1])*(gMean-greenAverages[1])+(bMean-greenAverages[2])*(bMean-greenAverages[2])) {
+		    		lcd.drawString("Blue",0,counter);counter++;
+		    	}
+		    	else {
+		    		lcd.drawString("Green",0,counter);counter++;
+		    	}
+		    }
+		    
+		    if(Math.abs(rMean-greenAverages[0])<=2*greenStd[0] &&  Math.abs(gMean-greenAverages[1])<=2*greenStd[1] && Math.abs(bMean-greenAverages[2])<=2*greenStd[2]) {
+		    	if(counter==1) {
+		    	if((rMean-blueAverages[0])*(rMean-blueAverages[0])+(gMean-blueAverages[1])*(gMean-blueAverages[1])+(bMean-blueAverages[2])*(bMean-blueAverages[2])
+		    			<(rMean-greenAverages[0])*(rMean-greenAverages[0])+(gMean-greenAverages[1])*(gMean-greenAverages[1])+(bMean-greenAverages[2])*(bMean-greenAverages[2])) {
+		    		lcd.drawString("Blue",0,counter);counter++;
+		    	}
+		    	else {
+		    		lcd.drawString("Green",0,counter);counter++;
+		    	}
+		    	} 
+		    }
+		    
+		    if(Math.abs(rMean-yellowAverages[0])<=2*yellowStd[0] &&  Math.abs(gMean-yellowAverages[1])<=2*yellowStd[1] && Math.abs(bMean-yellowAverages[2])<=2*yellowStd[2]) {
+		    	lcd.drawString("Yellow",0,counter);counter++;
+		    }
+		    
+		    if(Math.abs(rMean-redAverages[0])<=2*redStd[0] &&  Math.abs(gMean-redAverages[1])<=2*redStd[1] && Math.abs(bMean-redAverages[2])<=2*redStd[2]) {
+		    	lcd.drawString("Red",0,counter);counter++;
+		    }
+		    
+		    try{Thread.sleep(1600);}
+		    catch (Exception e){}
+		    colorMotor.rotate(-170,true);
+		    
+		  }   
+		  
+	  }
+
+}
---
+++ src/demo/Trial.java
@@ -0,0 +1,139 @@
+package demo;
+
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+/**
+ * Dummy class for testing stuff
+ * 
+ */
+public class Trial {
+
+    private static final int FORWARD_SPEED = 125;
+    
+    private static final int ROTATE_SPEED = 75 ;
+    private static final double TILE_SIZE = 30.48;
+
+    private static final EV3LargeRegulatedMotor leftMotor =new EV3LargeRegulatedMotor(LocalEV3.get().getPort("C"));
+    private static final EV3LargeRegulatedMotor rightMotor =new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+    public static final double radius = 2.093;
+    public static final double track = 12.45;
+    
+    private static Port portColor= LocalEV3.get().getPort("S2");
+    private static SensorModes myColor= new EV3ColorSensor(portColor);
+    private static SampleProvider myColorStatus = myColor.getMode("Red");
+    private static float[] sampleColor= new float[myColorStatus.sampleSize()]; 
+    private static int buttonChoice;
+    private static final long CORRECTION_PERIOD = 10;
+    private static final double LIGHT_THRESHOLD=0.05;
+    
+    private static Port portUS= LocalEV3.get().getPort("S1");
+	  private static SensorModes myUS =new EV3UltrasonicSensor(portUS);
+	  private static SampleProvider myDistance= myUS.getMode("Distance");
+	  private static float[] sampleUS= new float[myDistance.sampleSize()];
+
+    public static void main (String[] args)
+    {
+        leftMotor.setAcceleration(500);
+        rightMotor.setAcceleration(500);
+        System.out.println("READY");
+        
+        do { //wait until one of the buttons is pressed
+            buttonChoice = Button.waitForAnyPress(); 
+           
+          } while (buttonChoice != Button.ID_UP && buttonChoice != Button.ID_DOWN && buttonChoice != Button.ID_LEFT && buttonChoice != Button.ID_RIGHT
+        		  && buttonChoice != Button.ID_ENTER && buttonChoice != Button.ID_ESCAPE);
+        
+       
+        if (buttonChoice == Button.ID_UP) { // if up button, go forward 6 tiles
+        	
+           // leftMotor.setSpeed(FORWARD_SPEED);rightMotor.setSpeed(FORWARD_SPEED); leftMotor.forward();rightMotor.forward();
+        	leftMotor.setSpeed(FORWARD_SPEED);
+            rightMotor.setSpeed(FORWARD_SPEED);
+            leftMotor.rotate(convertDistance(radius, 6.0 * TILE_SIZE), true);
+            rightMotor.rotate(convertDistance(radius, 6.0 * TILE_SIZE), false);
+            Button.waitForAnyPress();  // if real distance bigger than 6 tiles, increase the constant radius (at the beginning of the file). If real distance smaller than 6 tiles, decrease radius
+        }
+        
+        if (buttonChoice == Button.ID_RIGHT) { //if right button, turn in place 1080 degrees to the right- 3tours- (do this trial once the you're statisfied with the 6-tile test
+        	leftMotor.setSpeed(ROTATE_SPEED);
+            rightMotor.setSpeed(ROTATE_SPEED);
+
+            leftMotor.rotate(convertAngle(radius, track, 1080.0), true);
+            rightMotor.rotate(-convertAngle(radius, track, 1080.0), false);
+            Button.waitForAnyPress(); // if angle bigger than 1080 degrees, decrease "track". If angle smaller than 1080 degrees, increase "track"
+        }
+        
+        
+        if (buttonChoice == Button.ID_DOWN) { //test the threshold required for line detection
+        	
+        	long correctionStart, correctionEnd;
+        myColorStatus.fetchSample(sampleColor,0);
+        float r1= sampleColor[0];
+        leftMotor.setSpeed(FORWARD_SPEED);
+        rightMotor.setSpeed(FORWARD_SPEED);
+        leftMotor.rotate(convertDistance(radius, 3.0 * TILE_SIZE), true); // go forward 3 tiles
+        rightMotor.rotate(convertDistance(radius, 3.0 * TILE_SIZE), true);
+        while(true)
+        {
+        	correctionStart = System.currentTimeMillis();
+        	
+        	myColorStatus.fetchSample(sampleColor,0);
+        	if(sampleColor[0]<r1-LIGHT_THRESHOLD) {Sound.beep();}
+        	r1=sampleColor[0];
+        	correctionEnd = System.currentTimeMillis();
+            if (correctionEnd - correctionStart < CORRECTION_PERIOD) {
+              try {
+                Thread.sleep(CORRECTION_PERIOD - (correctionEnd - correctionStart));
+              } catch (InterruptedException e) {
+                // there is nothing to be done here
+              }
+            }
+        }
+        }
+        
+        if (buttonChoice == Button.ID_LEFT)
+        {
+        	int wallDist;
+        	while(true) {
+        	myDistance.fetchSample(sampleUS,0);
+            wallDist= (int)(sampleUS[0]*100.0);
+            System.out.println(wallDist);
+        	}
+        }
+        
+        
+    }
+
+
+    /**
+     * This method allows the conversion of a distance to the total rotation of each wheel need to
+     * cover that distance.
+     * 
+     * @param radius the wheel radius in cm
+     * @param distance the distance that the wheel should move the robot in cm
+     * @return the number of degrees that the wheel will turn
+     */
+    private static int convertDistance(double radius, double distance) {
+        return (int) ((180.0 * distance) / (Math.PI * radius));
+    }
+
+    /**
+     * This method allows the conversion of an angle-turn to the total rotation of each wheel need to
+     * cover that distance.
+     * 
+     * @param radius the wheel radius
+     * @param width the length of the wheel axle
+     * @return the number of degrees that the wheel will turn
+     */
+    private static int convertAngle(double radius, double width, double angle) {
+        return convertDistance(radius, Math.PI * width * angle / 360.0);
+    }
+
+}
